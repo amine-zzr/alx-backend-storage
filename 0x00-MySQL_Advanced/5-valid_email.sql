d4c2257ea3812e5e4f080621c19ec91de3b83241
amine-zzr <aminezoukri@gmail.com>
2024-10-20 12:58:10 +0000

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Script to create a trigger that resets 'valid_email' to 0 only when the email has been changed.
+
+-- Drop the trigger if it already exists
+DROP TRIGGER IF EXISTS reset_valid_email_on_change;
+
+-- Create the trigger for resetting 'valid_email'
+CREATE TRIGGER reset_valid_email_on_change
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    -- Reset 'valid_email' to 0 if the email has been changed
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
