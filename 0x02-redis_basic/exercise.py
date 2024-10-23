32b9fc41dc0b87b643446e7a22781e930973954a
amine-zzr <aminezoukri@gmail.com>
2024-10-23 22:48:12 +0100

    fixing code

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -44,6 +44,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores the input data in Redis with a randomly generated key.
